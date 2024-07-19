48b1fe2e499c5ea8ba49565a69f4335855939ee5
rethabile <rethabilemakaotsi17@gmail.com>
2024-07-19 11:57:00 +0200

    displaying currencies

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
-import { createStore } from 'vuex'
+// store.js
+import { createStore } from "vuex";
+import axios from "axios";
 
-export default createStore({
+const store = createStore({
   state: {
-  },
-  getters: {
+    
+      currencies: [],
+    
   },
   mutations: {
+    setCurrencies(state, data) {
+      state.currencies = data;
+    },
   },
   actions: {
+    async fetchSpots({ commit }) {
+      try {
+        const response = await axios.get(
+          "https://api.sharenet.co.za/api/v1/px2/spots"
+        );
+        // console.log(response.data.spots);
+        const data = response.data.spots;
+        console.log(data);
+        commit("setCurrencies", data);
+      } catch (error) {
+        console.error("Error fetching spots:", error);
+      }
+    },
   },
-  modules: {
-  }
-})
+});
+
+export default store;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Currencies</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Full Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Move</th>
+          <th scope="col">Percentage Move</th>
+          <th scope="col">Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in currencies" :key="item.code">
+          <td>{{ item.fullName }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.move }}</td>
+          <td>{{ item.pmove.toFixed(2) }}</td>
+          <td>{{ new Date(item.datetime).toLocaleString() }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  computed: {
+    currencies() {
+      return this.$store.state.currencies
+    }
+  },
+  async created() {
+    await this.$store.dispatch('fetchSpots');
   }
-}
+};
 </script>
